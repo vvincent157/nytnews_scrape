490c347c990eed6a48e94acfdef3293636fdff6d
Victoria <Victoria@Victorias-MacBook-Pro.local>
2019-01-28 15:31:36 -0600

    about to deploy

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get('/home', function(req,res) {
+app.get('/', function(req,res) {
     res.render('home');
 })
 
